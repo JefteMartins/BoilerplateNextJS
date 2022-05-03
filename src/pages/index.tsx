f4cda6d0412d8d6c79e1312cce4956ec955a3020
JefteMartins <jefte.martins@hotmail.com>
2022-05-02 21:53:00 -0300

    update boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-
-
-export default function Home({ title = "React Avançado" }) {
+export default function Home({ title = 'React Avançado' }) {
   return (
     <div className={styles.container}>
       <Head>
